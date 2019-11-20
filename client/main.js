dcbc295c5a7a1364e57db066b2248419c160cdf1
Nirup Murali <Nirup@Skitters-MacBook-Pro-3.local>
2019-11-20 16:18:15 +0530

    init user crud

--- client/main.js
+++ client/main.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import {hydrate, render } from 'react-dom'
-import HelloWorld from './HelloWorld'
+import { render } from 'react-dom'
 import App from './App'
 
-
 render(<App/>, document.getElementById('root'))
-// hydrate(<App/>, document.getElementById('root'))
-// render(<HelloWorld/>, document.getElementById('root'))
